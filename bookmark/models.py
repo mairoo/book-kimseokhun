b00615ca1a629c611e46548e7555b8c1e929fa8a
mairoo <nasiajai@gmail.com>
2016-09-29 01:52:48 +0900

    bookmark 앱 추가

---
+++ bookmark/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    url = models.URLField('ulr', unique=True)
+
+    class Meta:
+        verbose_name = 'Bookmark'
+        verbose_name_plural = 'Bookmarks'
+
+    def __str__(self):
+        return self.title
